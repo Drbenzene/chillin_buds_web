e9eb99188e2a35ea297a31eb26756ae4c0cbc45c
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2023-07-19 04:48:02 -0500

    uii

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {}
 
 // module.exports = nextConfig
 
-// const withPWA = require('next-pwa');
-const withPWA = require("@ducanh2912/next-pwa").default()
+const withPWA = require('next-pwa');
+// const withPWA = require("@ducanh2912/next-pwa").default()
 
 module.exports = withPWA({
   dest: 'public', 
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  // disable: process.env.NODE_ENV === 'development',
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ function Index() {
           onChange={handleSearch}
           onKeyDown={handleKeyPress}
           value={search}
-          type="text" placeholder="Search" className="md:w-1/2 h-12 p-5 rounded-lg border-none outline-none focus:ring-2 focus:ring-green-800 focus:ring-opacity-50" />
+          type="text" placeholder="Search" className="md:w-1/2 mx-5  w-full h-12 p-5 rounded-lg border-none outline-none focus:ring-2 focus:ring-green-800 focus:ring-opacity-50" />
 
 
 
