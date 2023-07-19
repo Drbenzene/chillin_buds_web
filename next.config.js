b68b9e60f38d8f2f2c91f66aaef99e5ae581c71c
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2023-07-19 06:25:01 -0500

    update

--- next.config.js
+++ next.config.js
@@ -17,10 +17,10 @@ const runtimeCaching = require('next-pwa/cache')
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  disable: process.env.NODE_ENV === 'development',
+  // disable: process.env.NODE_ENV === 'development',
   skipWaiting: true,
   runtimeCaching,
-  disable: true,
+  disable: false,
   buildExcludes: [/middleware-manifest.json$/],
 })
 
