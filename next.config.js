9827c826bfdde215d351602b899bfaebbc6eb53c
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2023-07-18 19:36:06 -0500

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {}
 
 // module.exports = nextConfig
 
-const withPWA = require('next-pwa');
+// const withPWA = require('next-pwa');
+const withPWA = require("@ducanh2912/next-pwa").default()
 
 module.exports = withPWA({
   dest: 'public', 
